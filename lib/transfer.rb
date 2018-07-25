a880ce1bfdf515c9c99b44f87a925128c91c927d
Brenden Thornton <thorntonbrenden@gmail.com>
2018-07-25 22:54:54 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,13 +14,13 @@ class Transfer
   end 
   
   def execute_transaction
-    if valid? && sender.balance > amount && self.status == "pending"
-      sender.balance -= amount
-      receiver.balance += amount 
-      self.status = "complete"
-    elsif self.status = "rejected"
+    if self.status = "rejected"
     "Transaction rejected. Please check your account balance."
       
+    elsif valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount 
+      self.status = "complete"  
     end 
   end 
   
